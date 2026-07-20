606bbb2777b7ca6c5a5e9df922f95c976381453f
Sai Rupesh L <lsairupesh2311@gmail.com>
2026-07-20 11:57:43 +0530

    Update filter.c

--- filter.c
+++ filter.c
@@ -1,150 +1,239 @@
-#include <getopt.h>
-#include <stdio.h>
+#include "helpers.h"
+#include <math.h>
 #include <stdlib.h>
 
-#include "helpers.h"
-
-int main(int argc, char *argv[])
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Define allowable filters
-    char *filters = "begr";
-
-    // Get filter flag and check validity
-    char filter = getopt(argc, argv, filters);
-    if (filter == '?')
-    {
-        printf("Invalid filter.\n");
-        return 1;
-    }
-
-    // Ensure only one filter
-    if (getopt(argc, argv, filters) != -1)
-    {
-        printf("Only one filter allowed.\n");
-        return 2;
-    }
-
-    // Ensure proper usage
-    if (argc != optind + 2)
-    {
-        printf("Usage: ./filter [flag] infile outfile\n");
-        return 3;
-    }
-
-    // Remember filenames
-    char *infile = argv[optind];
-    char *outfile = argv[optind + 1];
-
-    // Open input file
-    FILE *inptr = fopen(infile, "r");
-    if (inptr == NULL)
-    {
-        printf("Could not open %s.\n", infile);
-        return 4;
-    }
-
-    // Open output file
-    FILE *outptr = fopen(outfile, "w");
-    if (outptr == NULL)
-    {
-        fclose(inptr);
-        printf("Could not create %s.\n", outfile);
-        return 5;
-    }
-
-    // Read infile's BITMAPFILEHEADER
-    BITMAPFILEHEADER bf;
-    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
-
-    // Read infile's BITMAPINFOHEADER
-    BITMAPINFOHEADER bi;
-    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
-
-    // Ensure infile is (likely) a 24-bit uncompressed BMP 4.0
-    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
-        bi.biBitCount != 24 || bi.biCompression != 0)
-    {
-        fclose(outptr);
-        fclose(inptr);
-        printf("Unsupported file format.\n");
-        return 6;
-    }
-
-    // Get image's dimensions
-    int height = abs(bi.biHeight);
-    int width = bi.biWidth;
-
-    // Allocate memory for image
-    RGBTRIPLE(*image)[width] = calloc(height, width * sizeof(RGBTRIPLE));
-    if (image == NULL)
-    {
-        printf("Not enough memory to store image.\n");
-        fclose(outptr);
-        fclose(inptr);
-        return 7;
-    }
-
-    // Determine padding for scanlines
-    int padding = (4 - (width * sizeof(RGBTRIPLE)) % 4) % 4;
-
-    // Iterate over infile's scanlines
+    // Loop over all pixels
     for (int i = 0; i < height; i++)
     {
-        // Read row into pixel array
-        fread(image[i], sizeof(RGBTRIPLE), width, inptr);
-
-        // Skip over padding
-        fseek(inptr, padding, SEEK_CUR);
-    }
-
-    // Filter image
-    switch (filter)
-    {
-        // Blur
-        case 'b':
-            blur(height, width, image);
-            break;
-
-        // Edges
-        case 'e':
-            edges(height, width, image);
-            break;
-
-        // Grayscale
-        case 'g':
-            grayscale(height, width, image);
-            break;
-
-        // Reflect
-        case 'r':
-            reflect(height, width, image);
-            break;
-    }
-
-    // Write outfile's BITMAPFILEHEADER
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
-
-    // Write outfile's BITMAPINFOHEADER
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
-
-    // Write new pixels to outfile
-    for (int i = 0; i < height; i++)
-    {
-        // Write row to outfile
-        fwrite(image[i], sizeof(RGBTRIPLE), width, outptr);
-
-        // Write padding at end of row
-        for (int k = 0; k < padding; k++)
+        for (int j = 0; j < width; j++)
         {
-            fputc(0x00, outptr);
+            // Take average of red, green, and blue
+            int avg =
+                round((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3.0);
+
+            // Update pixel values
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtGreen = avg;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Store the original values in temporary variables
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+            int originalRed = image[i][j].rgbtRed;
+
+            // Create variables to store sepia values
+            float sepiaRed = 0.0;
+            float sepiaBlue = 0.0;
+            float sepiaGreen = 0.0;
+
+            // Compute sepia values, Update pixel with sepia values
+            sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue;
+            sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue;
+            sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue;
+
+            // Round off
+            sepiaRed = round(sepiaRed);
+            sepiaGreen = round(sepiaGreen);
+            sepiaBlue = round(sepiaBlue);
+
+            // If result is above 255, make it 255
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+
+            // Change the image's pixel values to the sepia ones
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtRed = sepiaRed;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Loop over the left half of the image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - j - 1];
+            image[i][width - j - 1] = temp;
+        }
+    }
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Dynamically allocate memory for the copy on the heap
+    RGBTRIPLE(*copy)[width] = malloc(height * width * sizeof(RGBTRIPLE));
+    
+    // Safety check in case system is out of memory
+    if (copy == NULL)
+    {
+        return;
+    }
+
+    // Populate the copy
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
         }
     }
 
-    // Free memory for image
-    free(image);
+    // Calculate the new value for each pixel in the image using the copy
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Count the number of surrounding grids within image
+            int cnt = 0;
 
-    // Close files
-    fclose(inptr);
-    fclose(outptr);
-    return 0;
+            // Create temporary variables to store the new pixel values
+            float redtemp = 0.0;
+            float bluetemp = 0.0;
+            float greentemp = 0.0;
+
+            // Loop over the surrounding 3*3 grid and add all the values
+            for (int k = -1; k < 2; k++)
+            {
+                for (int l = -1; l < 2; l++)
+                {
+                    if ((-1 < (i + k)) && ((i + k) < height) && (-1 < (j + l)) && ((j + l) < width))
+                    {
+                        redtemp += copy[i + k][j + l].rgbtRed;
+                        bluetemp += copy[i + k][j + l].rgbtBlue;
+                        greentemp += copy[i + k][j + l].rgbtGreen;
+                        cnt += 1;
+                    }
+                }
+            }
+
+            // Calculate the average
+            redtemp /= (float) cnt;
+            bluetemp /= (float) cnt;
+            greentemp /= (float) cnt;
+
+            // Round off the values
+            redtemp = round(redtemp);
+            bluetemp = round(bluetemp);
+            greentemp = round(greentemp);
+
+            // Change the image pixel values to the new ones
+            image[i][j].rgbtRed = redtemp;
+            image[i][j].rgbtBlue = bluetemp;
+            image[i][j].rgbtGreen = greentemp;
+        }
+    }
+    
+    // Free the dynamically allocated memory before exiting
+    free(copy);
+    return;
+}
+
+// Edge Detection
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Dynamically allocate memory for the copy on the heap
+    RGBTRIPLE(*copy)[width] = malloc(height * width * sizeof(RGBTRIPLE));
+    
+    // Safety check
+    if (copy == NULL)
+    {
+        return;
+    }
+
+    // Populate the copy
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    // Define the Sobel Kernels
+    int Gx[3][3] = {
+        {-1, 0, 1},
+        {-2, 0, 2},
+        {-1, 0, 1}
+    };
+    int Gy[3][3] = {
+        {-1, -2, -1},
+        { 0,  0,  0},
+        { 1,  2,  1}
+    };
+
+    // Calculate the new value for each pixel in the image using the copy
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Initialize accumulators for Red, Green, Blue
+            float Gx_red = 0, Gx_green = 0, Gx_blue = 0;
+            float Gy_red = 0, Gy_green = 0, Gy_blue = 0;
+
+            // Loop over the surrounding 3*3 grid
+            for (int k = -1; k < 2; k++)
+            {
+                for (int l = -1; l < 2; l++)
+                {
+                    if ((-1 < (i + k)) && ((i + k) < height) && (-1 < (j + l)) && ((j + l) < width))
+                    {
+                        Gx_red   += copy[i + k][j + l].rgbtRed   * Gx[k + 1][l + 1];
+                        Gx_green += copy[i + k][j + l].rgbtGreen * Gx[k + 1][l + 1];
+                        Gx_blue  += copy[i + k][j + l].rgbtBlue  * Gx[k + 1][l + 1];
+
+                        Gy_red   += copy[i + k][j + l].rgbtRed   * Gy[k + 1][l + 1];
+                        Gy_green += copy[i + k][j + l].rgbtGreen * Gy[k + 1][l + 1];
+                        Gy_blue  += copy[i + k][j + l].rgbtBlue  * Gy[k + 1][l + 1];
+                    }
+                }
+            }
+
+            // Calculate the new values and round off
+            int redtemp   = round(sqrt(Gx_red * Gx_red + Gy_red * Gy_red));
+            int greentemp = round(sqrt(Gx_green * Gx_green + Gy_green * Gy_green));
+            int bluetemp  = round(sqrt(Gx_blue * Gx_blue + Gy_blue * Gy_blue));
+
+            // Cap values at 255 and change the image pixel values
+            image[i][j].rgbtRed   = (redtemp > 255) ? 255 : redtemp;
+            image[i][j].rgbtGreen = (greentemp > 255) ? 255 : greentemp;
+            image[i][j].rgbtBlue  = (bluetemp > 255) ? 255 : bluetemp;
+        }
+    }
+    
+    // Free the dynamically allocated memory before exiting
+    free(copy);
+    return;
 }
